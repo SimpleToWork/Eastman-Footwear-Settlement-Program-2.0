f3daaa1899ada1e2833a00667c77b841efb7167b
Ricky Schweky <Ricky@Simpletowork.com>
2023-10-17 06:58:25 -0400

    updates

--- Batch Files/configure.bat
+++ Batch Files/configure.bat
@@ -1,12 +1,12 @@
 @echo off
 
-cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program"
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0"
 python -m virtualenv venv
 
 cd venv/scripts
 call activate.bat
 
-cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program"
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0"
 pip install -r requirements.txt
 
 cmd /kcd
