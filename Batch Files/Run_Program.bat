945a48217cdcb1569f5c97473574b6630bfc5750
Ricky Schweky <Ricky@Simpletowork.com>
2023-10-17 07:03:10 -0400

    updates

--- Batch Files/Run_Program.bat
+++ Batch Files/Run_Program.bat
@@ -1,5 +1,6 @@
 cd C:\
-python "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\Python Files\RLM_Data_Import.py"
-python "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\Python Files\Settlement_Conversion_Program.py"
+"C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\Python Files\RLM_Data_Import.py"
+"C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\venv\scripts\python.exe" "C:\Users\%USERNAME%\Desktop\New Projects\Eastman Footwear\Eastman-Footwear-Settlement-Program-2.0\Python Files\Settlement_Conversion_Program.py"
+
 
 cmd/k
